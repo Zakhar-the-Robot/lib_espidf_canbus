7e85fdfbea90627b778af48e1cc9fcd73d000f19
Andrei Gramakov <mail@agramakov.me>
2022-07-01 16:09:50 +0200

    Update examples

--- examples/node_first/main/main.cpp
+++ examples/node_first/main/main.cpp
@@ -1,6 +1,6 @@
 // *************************************************************************
 //
-// Copyright (c) 2021 Andrei Gramakov. All rights reserved.
+// Copyright (c) 2022 Andrei Gramakov. All rights reserved.
 //
 // This file is licensed under the terms of the MIT license.
 // For a copy, see: https://opensource.org/licenses/MIT
@@ -10,92 +10,49 @@
 //
 // *************************************************************************
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "canbus.hpp"
+#include "delay.h" // from canbus lib
 #include "esp_err.h"
 #include "esp_log.h"
-#include "canbus.hpp"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "millis.h" // from canbus lib
-#include "delay.h"  // from canbus lib
+#include <stdio.h>
+#include <stdlib.h>
 
 #define TAG "first"
 #define TX_GPIO_NUM static_cast<gpio_num_t>(21)
 #define RX_GPIO_NUM static_cast<gpio_num_t>(22)
-#define DATA_PERIOD_MS 5000
-#define ID_2ND_STORE 0x0c5
-#define ID_MASTER_RQ 0x0A5
+#define DATA_PERIOD_MS 2000
+#define DEVICE_ID_2ND 0x2
+#define DEVICE_ID_3RD 0x3
 
-static twai_message_t msg_to_store = {.identifier = ID_2ND_STORE,
-                                    .data_length_code = 8,
-                                    .data = {1, 2, 3, 4, 5, 6, 7, 8}};
-static twai_message_t msg_for_response = {};
+static uint8_t cmd[] = { 0xA0, 2, 3, 4, 5, 6, 7, 8 };
+
+void RxCallback(CanBus* dev, twai_message_t& rMsg)
+{
+    ESP_LOGI(TAG, "[<] Message 7...0: \tID:%03x - %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x", rMsg.identifier,rMsg.data[7], rMsg.data[6],
+             rMsg.data[5], rMsg.data[4], rMsg.data[3], rMsg.data[2], rMsg.data[1], rMsg.data[0]);
+}
 
 extern "C" void app_main(void)
 {
+    // esp_log_level_set("CAN", ESP_LOG_DEBUG);
+
     devCanBus.Start(1U, TX_GPIO_NUM, RX_GPIO_NUM);
     esp_err_t res;
-    while (devCanBus.IsStarted())
-    {
-        ESP_LOGI(TAG,"-------------------------------------------------------");
-        res = devCanBus.Send(msg_to_store);
-        ESP_LOGI(TAG, "Sent data to store: id:0x%x, %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x -> res:0x%x",
-                 msg_to_store.identifier,
-                 msg_to_store.data[0],
-                 msg_to_store.data[1],
-                 msg_to_store.data[2],
-                 msg_to_store.data[3],
-                 msg_to_store.data[4],
-                 msg_to_store.data[5],
-                 msg_to_store.data[6],
-                 msg_to_store.data[7],
-                 res);
-        msg_to_store.data[0]++;
-
-        ESP_LOGI(TAG,"-------------------------------------------------------");
-        ESP_LOGI(TAG, "Request data, 2nd aware of (id:0x%02x)", ID_MASTER_RQ);
-        res = devCanBus.Request(ID_MASTER_RQ, msg_for_response, 100);
-        if (res == ESP_OK)
-        {
-            ESP_LOGI(TAG, "Got requested data (id:0x%x, %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x)",
-                     msg_for_response.identifier,
-                     msg_for_response.data[0],
-                     msg_for_response.data[1],
-                     msg_for_response.data[2],
-                     msg_for_response.data[3],
-                     msg_for_response.data[4],
-                     msg_for_response.data[5],
-                     msg_for_response.data[6],
-                     msg_for_response.data[7]);
-        }
-        else
-        {
-            ESP_LOGE(TAG, "Error! (res:0x%x)", res);
-        }
+    devCanBus.SetCallbackRxData(RxCallback);
+    while (devCanBus.IsStarted()) {
+        res = devCanBus.SendCmdMsg(DEVICE_ID_2ND, cmd[7]++, cmd[6]++, cmd[5]++, cmd[4]++, cmd[3]++, cmd[2]++,
+                                   cmd[1]++, cmd[0]++);
+        ESP_LOGW(TAG, "[-> Cmd: target_dev_id:0x%x, %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x -> res:0x%x",
+                 DEVICE_ID_2ND, cmd[7], cmd[6], cmd[5], cmd[4], cmd[3], cmd[2], cmd[1], cmd[0], res);
         delay(DATA_PERIOD_MS);
 
-        ESP_LOGI(TAG,"-------------------------------------------------------");
-        ESP_LOGW(TAG, "Request data, 2nd not aware of (id:0x%02x)", ID_MASTER_RQ+1);
-        res = devCanBus.Request(ID_MASTER_RQ + 1, msg_for_response, 100);
-        if (res == ESP_OK)
-        {
-            ESP_LOGI(TAG, "Got requested data (id:0x%x, %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x)",
-                     msg_for_response.identifier,
-                     msg_for_response.data[0],
-                     msg_for_response.data[1],
-                     msg_for_response.data[2],
-                     msg_for_response.data[3],
-                     msg_for_response.data[4],
-                     msg_for_response.data[5],
-                     msg_for_response.data[6],
-                     msg_for_response.data[7]);
-        }
-        else
-        {
-            ESP_LOGE(TAG, "Error! (res:0x%x)", res);
-        }
-
+        res = devCanBus.SendCmdMsg(DEVICE_ID_3RD, cmd[7]++, cmd[6]++, cmd[5]++, cmd[4]++, cmd[3]++, cmd[2]++,
+                                   cmd[1]++, cmd[0]++);;
+        ESP_LOGI(TAG, "[-> Cmd: target_dev_id:0x%x, %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x -> res:0x%x",
+                 DEVICE_ID_3RD, cmd[7], cmd[6], cmd[5], cmd[4], cmd[3], cmd[2], cmd[1], cmd[0], res);
         delay(DATA_PERIOD_MS);
     }
 }
