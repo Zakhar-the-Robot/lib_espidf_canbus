312b5026ef4766f1f67b4973eab0632f6dc48f8f
Andrei Gramakov <mail@agramakov.me>
2022-07-01 16:51:04 +0200

    Feature/12 first release (#13)
    * Update supporting files
    
    * Update the library API
    
    * Update examples
    
    * Update readme, remove obsolete files
    
    * Update documentation

--- examples/node_second/main/main.cpp
+++ examples/node_second/main/main.cpp
@@ -1,6 +1,6 @@
 // *************************************************************************
 //
-// Copyright (c) 2021 Andrei Gramakov. All rights reserved.
+// Copyright (c) 2022 Andrei Gramakov. All rights reserved.
 //
 // This file is licensed under the terms of the MIT license.
 // For a copy, see: https://opensource.org/licenses/MIT
@@ -10,14 +10,14 @@
 //
 // *************************************************************************
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_err.h"
-#include "esp_log.h"
 #include "canbus.hpp"
 #include "delay.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 #define TAG "second"
 #define TX_GPIO_NUM static_cast<gpio_num_t>(21)
@@ -25,58 +25,60 @@
 #define DATA_PERIOD_MS 1500
 #define ID_MASTER_DATA 0x0A4
 
+static uint8_t data_storage[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+static uint8_t counter = 0;
+
+void RxCallback(CanBus* dev, twai_message_t& rMsg)
+{
+    ESP_LOGI(TAG, "[<] Message 7...0: \tID:%03x - %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x", rMsg.identifier,
+             rMsg.data[7], rMsg.data[6], rMsg.data[5], rMsg.data[4], rMsg.data[3], rMsg.data[2], rMsg.data[1],
+             rMsg.data[0]);
+}
+
+void TxCallback(CanBus* dev, twai_message_t& rMsg)
+{
+    ESP_LOGI(TAG,
+             "[>] Tx Message before callback 7...0: \tID:%03x - %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x",
+             rMsg.identifier, rMsg.data[7], rMsg.data[6], rMsg.data[5], rMsg.data[4], rMsg.data[3],
+             rMsg.data[2], rMsg.data[1], rMsg.data[0]);
+    rMsg.data[0] += 0x12;
+    rMsg.data[2] = counter++;
+    ESP_LOGI(TAG, "[-> Tx Message to send         7...0: \tID:%03x - %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x",
+             rMsg.identifier, rMsg.data[7], rMsg.data[6], rMsg.data[5], rMsg.data[4], rMsg.data[3],
+             rMsg.data[2], rMsg.data[1], rMsg.data[0]);
+}
+
+void RxCmdCallback(CanBus* dev, twai_message_t& rMsg)
+{
+    ESP_LOGW(TAG, "[!] Command 7...0: \tID:%03x - %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x", rMsg.identifier,
+             rMsg.data[7], rMsg.data[6], rMsg.data[5], rMsg.data[4], rMsg.data[3], rMsg.data[2], rMsg.data[1],
+             rMsg.data[0]);
+}
+
 extern "C" void app_main(void)
 {
+
+    // esp_log_level_set("CAN", ESP_LOG_DEBUG);
+
     ESP_LOGI(TAG, "-------------------------------------------------------");
     ESP_LOGI(TAG, "CAN start...");
     devCanBus.Start(2U, TX_GPIO_NUM, RX_GPIO_NUM);
 
-
     ESP_LOGI(TAG, "-------------------------------------------------------");
     ESP_LOGI(TAG, "Setting up the periodic TX...");
-    uint8_t tx_source = 0xb;
-    CanBusDataPointers_t tx_data = {};
-    tx_data.d0 = &tx_source;
-    devCanBus.AddTxDescriptor(0xb5, tx_data);
+    CanBusDataPointers_t tx_data
+        = { data_storage, data_storage + 1, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr };
+    devCanBus.AddTxDescriptor(0xa, tx_data);
+    devCanBus.AddTxDescriptor(0x1, tx_data);
     devCanBus.SetTxPeriod(3000);
+    devCanBus.SetCallbackTx(TxCallback);
 
     ESP_LOGI(TAG, "-------------------------------------------------------");
-    ESP_LOGI(TAG, "Setting up the Store on receiveing...");
-    uint8_t data_storage[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    CanBusDataPointers_t store_data = {};
-    store_data.d0 = &data_storage[0];
-    store_data.d1 = &data_storage[1];
-    store_data.d2 = &data_storage[2];
-    store_data.d3 = &data_storage[3];
-    store_data.d4 = &data_storage[4];
-    store_data.d5 = &data_storage[5];
-    store_data.d6 = &data_storage[6];
-    store_data.d7 = &data_storage[7];
-    devCanBus.AddStoreDescriptor(0xc5, store_data);
-    
-    ESP_LOGI(TAG, "-------------------------------------------------------");
-    ESP_LOGI(TAG, "Setting up the response...");
-    CanBusDataPointers_t resp_data = {};
-    resp_data.d0 = &tx_source;
-    resp_data.d2 = &data_storage[0];
-    resp_data.d3 = &data_storage[1];
-    resp_data.d4 = &data_storage[2];
-    devCanBus.AddResponseDescriptor(0xa5, resp_data);
+    ESP_LOGI(TAG, "Setting up the Store on receiving...");
+    devCanBus.SetCallbackRx(RxCallback);
+    devCanBus.SetCallbackRxCmd(RxCmdCallback);
 
-
-
-    while (devCanBus.IsStarted())
-    {
-        ESP_LOGI(TAG, "Storage: \t %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x",
-                 data_storage[0],
-                 data_storage[1],
-                 data_storage[2],
-                 data_storage[3],
-                 data_storage[4],
-                 data_storage[5],
-                 data_storage[6],
-                 data_storage[7]);
-        tx_source++;
-        delay(100);
+    while (1) {
+        delay(0U);
     }
 }
--- include/canbus_version.hpp
+++ include/canbus_version.hpp
@@ -1,6 +1,6 @@
 // *************************************************************************
 //
-// Copyright (c) 2021 Andrei Gramakov. All rights reserved.
+// Copyright (c) 2022 Andrei Gramakov. All rights reserved.
 //
 // This file is licensed under the terms of the MIT license.
 // For a copy, see: https://opensource.org/licenses/MIT
@@ -10,16 +10,14 @@
 //
 // *************************************************************************
 
-#pragma once
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdint.h>
 
-uint64_t millis();
+// Library version
+const unsigned char VERSION_MAJOR = 1;
+const unsigned char VERSION_MINOR = 0;
+const unsigned char VERSION_PATCH = 0;
 
-#ifdef __cplusplus
-}
-#endif
+// Protocol version
+const unsigned char QCAN_VERSION_MAJOR = 0;
+const unsigned char QCAN_VERSION_MINOR = 2;
+const unsigned char QCAN_VERSION_PATCH = 0;
